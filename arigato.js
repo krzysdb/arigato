72a84fa02089b9e04d09ff3c9beceb5ae137a61d
Laura Zamanillo <laura.zamanillo@gmai.com>
2011-08-06 15:57:39 +0100

    Onclick events added.

--- arigato.js
+++ arigato.js
@@ -1,10 +1,12 @@
 
 var katakana = [
-	{consonant:'a',character:"あ"},
-	{consonant:'i',character:"い"},
-	{consonant:'u',character:"う"},
-	{consonant:'ka',character:"か"}
-];
+            	{consonant:'a',character:"あ"},
+            	{consonant:'i',character:"い"},
+            	{consonant:'u',character:"う"},
+            	{consonant:'ka',character:"か"}
+            ];
+
+
 
 function populate(){
 	var el = document.getElementById("sounds");
@@ -12,8 +14,22 @@ function populate(){
 	for(var i=0; i<katakana.length; i++){
 		var li = document.createElement('li');
 		li.innerHTML = katakana[i].consonant;
+		li.onclick = function(){
+			var self = this;
+			alert(self.innerHTML);
+		};
 		el.appendChild(li);
+		
+	}
+	var selectedLetters = document.getElementsByClassName('movingLetter');
+	for(var i=0; i<selectedLetters.length; i++){
+		selectedLetters[i].onclick = function(){
+			alert('wow');
+		};
 	}
 	
 	
-}
+	
+};
+	
+
