91b9c5a6c704efcd6d16e193b88d2ebc24d40b38
Laura Zamanillo <laura.zamanillo@gmai.com>
2011-08-06 17:11:15 +0100

    Testing local storage

--- arigato.js
+++ arigato.js
@@ -4,11 +4,19 @@ var katakana = [
             	{consonant:'i',character:"い"},
             	{consonant:'u',character:"う"},
             	{consonant:'ka',character:"か"}
-            ];
+];
 
-var selectedSound = movingLetter = '';
+var selectedSound = movingLetter ='';
 
 function populate(){
+	
+	//we populate the value of one letter moving down. (PLEASE NOTE INDEX HARCODED TO 1)
+	var selected = document.getElementById('selected');
+	selected.innerHTML = katakana[1].character;
+	
+	//we store that value in the local storage
+	window.localStorage.setItem('value', 1);
+	
 	var el = document.getElementById("sounds");
 	
 	for(var i=0; i<katakana.length; i++){
@@ -16,42 +24,46 @@ function populate(){
 		li.innerHTML = katakana[i].consonant;
 		li.onclick = function(){
 			var self = this;
-			alert(self.innerHTML);
 			
-		    window.localStorage.setItem('sound', self.innerHTML);
-		    
-		    areTheyTheSame();
-		    
-		    //window.localStorage.setItem('timestamp', (new Date()).getTime());
+			//var selectedSound = window.localStorage.getItem('sound');
 			
+		    //window.localStorage.setItem('sound', self.innerHTML);
+		    
+		    areTheyTheSame(self.innerHTML);
+		
 		};
 		el.appendChild(li);
 		
 	}
 	
-	var selectedLetters = document.getElementsByClassName('movingLetter');
+	/*var selectedLetters = document.getElementsByClassName('movingLetter');
 	for(var i=0; i<selectedLetters.length; i++){
 		selectedLetters[i].onclick = function(){
-			var self = this;
-			selectedSound = window.localStorage.getItem('sound');
 			
-			window.localStorage.setItem('fallingLetter', self.innerHTML);
-			
-			areTheyTheSame();
 		};
+	}*/
+	
+
+};
+
+function areTheyTheSame(selectedSound){
+	var index = window.localStorage.getItem('value');
+	alert(katakana[index].sound +' '+ selectedSound);
+	
+	if (katakana[index].sound == selectedSound){
+		alert('wow');
+	}else{
+		alert('keep trying');
 	}
 	
 	
-	
-};
-
-function areTheyTheSame(){
-	if (selectedSound == movingLetter){
+	/*if (selectedSound == movingLetter){
 		alert('wow');
 		selectSound = movingLetter = '';
 	}else{
 		alert('keep trying');
-	}
+	}*/
+	
 }
 	
 
